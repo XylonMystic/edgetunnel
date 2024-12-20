0cec89510544e92efd3ccaf1cc45777468ee4bf5
Nf_98999881 <43160730+XylonMystic@users.noreply.github.com>
2024-12-20 21:43:37 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = 's10.serv00.com:15661';
+let proxyIP = 's1.serv00.com:52001';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
